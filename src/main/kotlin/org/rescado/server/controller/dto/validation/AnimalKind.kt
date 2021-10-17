e6452aaadc2f68c910041bae82dcb58c1e683cf1
Kristof Dewilde <k.dewilde@napoleongames.be>
2021-10-17 22:34:48 +0200

    Added animal kind/sex validators

---
+++ src/main/kotlin/org/rescado/server/controller/dto/validation/AnimalKind.kt
@@ -0,0 +1,25 @@
+package org.rescado.server.controller.dto.validation
+
+import org.rescado.server.persistence.entity.Animal
+import javax.validation.Constraint
+import javax.validation.ConstraintValidator
+import javax.validation.ConstraintValidatorContext
+import kotlin.reflect.KClass
+
+@Target(AnnotationTarget.PROPERTY_GETTER)
+@Retention(AnnotationRetention.RUNTIME)
+@Constraint(validatedBy = [AnimalKindValidator::class])
+annotation class AnimalKind(
+    val message: String = "{validation.AnimalKind.message}",
+    val groups: Array<KClass<out Any>> = [],
+    val payload: Array<KClass<out Any>> = [],
+)
+
+class AnimalKindValidator : ConstraintValidator<AnimalKind, String> {
+
+    override fun initialize(constraintAnnotation: AnimalKind) {}
+
+    override fun isValid(value: String?, context: ConstraintValidatorContext?): Boolean {
+        return enumValues<Animal.Kind>().any { it.name.equals(value, true) }
+    }
+}
---
+++ src/main/kotlin/org/rescado/server/controller/dto/validation/AnimalSex.kt
@@ -0,0 +1,25 @@
+package org.rescado.server.controller.dto.validation
+
+import org.rescado.server.persistence.entity.Animal
+import javax.validation.Constraint
+import javax.validation.ConstraintValidator
+import javax.validation.ConstraintValidatorContext
+import kotlin.reflect.KClass
+
+@Target(AnnotationTarget.PROPERTY_GETTER)
+@Retention(AnnotationRetention.RUNTIME)
+@Constraint(validatedBy = [AnimalSexValidator::class])
+annotation class AnimalSex(
+    val message: String = "{validation.AnimalSex.message}",
+    val groups: Array<KClass<out Any>> = [],
+    val payload: Array<KClass<out Any>> = [],
+)
+
+class AnimalSexValidator : ConstraintValidator<AnimalSex, String> {
+
+    override fun initialize(constraintAnnotation: AnimalSex) {}
+
+    override fun isValid(value: String?, context: ConstraintValidatorContext?): Boolean {
+        return enumValues<Animal.Sex>().any { it.name.equals(value, true) }
+    }
+}
