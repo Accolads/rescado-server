ab7630c5806f4ed1493dae833896626881c296f9
Kristof Dewilde <k.dewilde@napoleongames.be>
2022-01-05 13:18:15 +0100

    Updated ExceptionResolver to handle DataIntegrityVioletions better
    Instead of throwing a generic exception we can try and look into the causes to identify an exception we can handle more nicely. The PSQLException because value too long is not a solution however. We need to add @Size constraints in our request DTOs.

--- src/main/kotlin/org/rescado/server/controller/ExceptionResolver.kt
+++ src/main/kotlin/org/rescado/server/controller/ExceptionResolver.kt
@@ -3,12 +3,15 @@ package org.rescado.server.controller
 import io.jsonwebtoken.ExpiredJwtException
 import io.jsonwebtoken.MalformedJwtException
 import io.jsonwebtoken.security.SignatureException
+import org.postgresql.util.PSQLException
 import org.rescado.server.controller.dto.build
+import org.rescado.server.controller.dto.res.Response
 import org.rescado.server.controller.dto.res.error.BadRequest
 import org.rescado.server.controller.dto.res.error.Conflict
 import org.rescado.server.controller.dto.res.error.InternalServerError
 import org.rescado.server.controller.dto.res.error.MethodNotAllowed
 import org.rescado.server.controller.dto.res.error.NotFound
+import org.rescado.server.controller.dto.res.error.PayloadTooLarge
 import org.rescado.server.controller.dto.res.error.Unauthorized
 import org.rescado.server.filter.exception.MalformedCredentialsException
 import org.rescado.server.filter.exception.MissingCredentialsException
@@ -18,6 +21,7 @@ import org.rescado.server.service.exception.ImageSourceException
 import org.rescado.server.service.exception.PhotoMaximumLimitReachedException
 import org.rescado.server.service.exception.PhotoMinimumLimitReachedException
 import org.springframework.dao.DataIntegrityViolationException
+import org.springframework.http.ResponseEntity
 import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException
 import org.springframework.security.web.firewall.RequestRejectedException
@@ -36,79 +40,88 @@ class ExceptionResolver(
 ) {
 
     @ExceptionHandler
-    fun handleImageSourceException(e: ImageSourceException) =
+    fun resolve(e: ImageSourceException) =
         BadRequest(error = messageService["exception.ImageSourceException.message", e.type.name.lowercase()]).build()
 
     @ExceptionHandler
-    fun handlePhotoMinimumLimitReachedException(e: PhotoMinimumLimitReachedException) =
+    fun resolve(e: PhotoMinimumLimitReachedException) =
         Conflict(error = messageService["exception.PhotoMinimumLimitReachedException.message", e.limit]).build()
 
     @ExceptionHandler
-    fun handlePhotoMaximumLimitReachedException(e: PhotoMaximumLimitReachedException) =
+    fun resolve(e: PhotoMaximumLimitReachedException) =
         Conflict(error = messageService["exception.PhotoMaximumLimitReachedException.message", e.limit]).build()
 
     @ExceptionHandler
-    fun handleHttpMessageNotReadableException(e: HttpMessageNotReadableException) =
+    fun resolve(e: HttpMessageNotReadableException) =
         BadRequest(error = messageService["exception.HttpMessageNotReadableException.message"]).build()
 
     @ExceptionHandler
-    fun handleHttpMediaTypeNotSupportedException(e: HttpMediaTypeNotSupportedException) =
+    fun resolve(e: HttpMediaTypeNotSupportedException) =
         BadRequest(error = messageService["exception.HttpMediaTypeNotSupportedException.message", e.contentType.toString()]).build()
 
     @ExceptionHandler
-    fun handleMissingRequestHeaderException(e: MissingRequestHeaderException) =
+    fun resolve(e: MissingRequestHeaderException) =
         BadRequest(error = messageService["exception.MissingRequestHeaderException.message", e.headerName]).build()
 
     @ExceptionHandler
-    fun handleNoHandlerFoundException(e: NoHandlerFoundException) =
+    fun resolve(e: NoHandlerFoundException) =
         NotFound(error = messageService["exception.NoHandlerFoundException.message"]).build()
 
     @ExceptionHandler
-    fun handleMethodArgumentTypeMismatchException(e: MethodArgumentTypeMismatchException) =
+    fun resolve(e: MethodArgumentTypeMismatchException) =
         BadRequest(error = messageService["exception.MethodArgumentTypeMismatchException.message", e.value]).build()
 
     @ExceptionHandler
-    fun handleHttpRequestMethodNotSupportedException(e: HttpRequestMethodNotSupportedException) =
+    fun resolve(e: HttpRequestMethodNotSupportedException) =
         MethodNotAllowed(error = messageService["exception.HttpRequestMethodNotSupportedException.message", e.method]).build()
 
     @ExceptionHandler
-    fun handleRequestRejectedException(e: RequestRejectedException) =
+    fun resolve(e: RequestRejectedException) =
         BadRequest(error = messageService["exception.RequestRejectedException.message"]).build()
 
     @ExceptionHandler
-    fun handleAuthenticationCredentialsNotFoundException(e: AuthenticationCredentialsNotFoundException, req: HttpServletRequest) =
+    fun resolve(e: AuthenticationCredentialsNotFoundException, req: HttpServletRequest) =
         Unauthorized(error = messageService["exception.AuthenticationCredentialsNotFoundException.message"], reason = Unauthorized.Reason.INVALID_CREDENTIALS, realm = req.serverName).build()
 
     @ExceptionHandler
-    fun handleMissingCredentialsException(e: MissingCredentialsException, req: HttpServletRequest) =
+    fun resolve(e: MissingCredentialsException, req: HttpServletRequest) =
         Unauthorized(error = messageService["exception.MissingCredentialsException.message"], reason = Unauthorized.Reason.NO_CREDENTIALS, realm = req.serverName).build()
 
     @ExceptionHandler
-    fun handleUnsupportedCredentialsException(e: UnsupportedCredentialsException, req: HttpServletRequest) =
+    fun resolve(e: UnsupportedCredentialsException, req: HttpServletRequest) =
         Unauthorized(error = messageService["exception.UnsupportedCredentialsException.message"], reason = Unauthorized.Reason.INVALID_CREDENTIALS, realm = req.serverName).build()
 
     @ExceptionHandler
-    fun handleMalformedCredentialsException(e: MalformedCredentialsException, req: HttpServletRequest) =
+    fun resolve(e: MalformedCredentialsException, req: HttpServletRequest) =
         Unauthorized(error = messageService["exception.MalformedCredentialsException.message"], reason = Unauthorized.Reason.MALFORMED_CREDENTIALS, realm = req.serverName).build()
 
     @ExceptionHandler
-    fun handleMalformedJwtException(e: MalformedJwtException, req: HttpServletRequest) =
+    fun resolve(e: MalformedJwtException, req: HttpServletRequest) =
         Unauthorized(error = messageService["exception.MalformedJwtException.message"], reason = Unauthorized.Reason.MALFORMED_CREDENTIALS, realm = req.serverName).build()
 
     @ExceptionHandler
-    fun handleSignatureException(e: SignatureException, req: HttpServletRequest) =
+    fun resolve(e: SignatureException, req: HttpServletRequest) =
         Unauthorized(error = messageService["exception.SignatureException.message"], reason = Unauthorized.Reason.INVALID_ACCESS_TOKEN, realm = req.serverName).build()
 
     @ExceptionHandler
-    fun handleExpiredJwtException(e: ExpiredJwtException, req: HttpServletRequest) =
+    fun resolve(e: ExpiredJwtException, req: HttpServletRequest) =
         Unauthorized(error = messageService["exception.ExpiredJwtException.message"], reason = Unauthorized.Reason.EXPIRED_ACCESS_TOKEN, realm = req.serverName).build()
 
     @ExceptionHandler
-    fun handleDataIntegrityViolationException(e: DataIntegrityViolationException) =
-        InternalServerError(error = messageService["exception.DataIntegrityViolationException.message"]).build()
+    fun resolve(e: DataIntegrityViolationException): ResponseEntity<Response> {
+        var cause: Throwable? = e
+        var limit = 10
+        while (cause != null && limit != 0) {
+            if (cause is PSQLException && cause.message?.contains("value too long for type") == true)
+                return PayloadTooLarge(error = messageService["exception.PSQLExceptionTooLong.message"]).build()
+            cause = cause.cause
+            limit--
+        }
+        return InternalServerError(error = messageService["exception.DataIntegrityViolationException.message"]).build()
+    }
 
     @ExceptionHandler
-    fun handleException(e: Exception) =
+    fun resolve(e: Exception) =
         InternalServerError(
             errors = mutableListOf(messageService["exception.Exception.message"]).apply {
                 e.printStackTrace()
